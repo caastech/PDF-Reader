00c3750f21386b62e0c2071b39cdee806daf1915
Clemente Acosta <clems.acosta@gmail.com>
2021-11-05 16:28:30 -0500

    Fixed Bug with the blank inputs
    Removed and renamed getContent function to the app.js file, added all the logic necessary to this change

--- app.js
+++ app.js
@@ -2,24 +2,19 @@ const clkApp = Vue.createApp({
 
     template: 
     /*html*/
-    `   
-        {{ pdfData }}
-        <br>
-        
-        <custom-input @get-data="getContent" id="clkFile" type="file" title="Please enter your PDF file" 
-        :handler="true" 
-        ></custom-input>
-
-        <custom-input v-for="(field,key) in fields" 
-        :key="key" 
-        :id="field.id"
-        :type="field.type"
-        :title="field.title"
-        :customValue="pdfData[field.id]"
-        ></custom-input>
-
-
+    `
+    <label for="clkFile">Please enter your PDF file</label><br>
+    <input @change="getPdfData" type="file" name="clk" id="clkFile" class="mb-2"><br>
+    
+    
+    <custom-input v-if="isChanged" v-for="(field,key) in fields" 
+    :key="key" 
+    :id="field.id"
+    :title="field.title"
+    :customValue="pdfData[field.id]"
+    ></custom-input>
     `,
+
 
     data(){
         return{
@@ -29,16 +24,111 @@ const clkApp = Vue.createApp({
                 { id:'clientPay', type:'text', title:'Client Pay'},
             ],
             pdfData: [],
-            msg: 'Here I am'
+            msg: 'Here I am',
+            isChanged: false,
         }
     },
 
     methods: {
-        getContent(pdf){
-            this.pdfData = {
-                ...pdf
-            }
-            console.log('PDF Data:');
+
+        async getPdfData(event){
+            console.log('Changed', event.target.files[0].name);
+            
+            // Getting the pdf file pages as a promise
+            const doc = await pdfjsLib.getDocument(`special/${event.target.files[0].name}`).promise // note the use of the property promise
+
+            // Getting the first page of the file
+            const page = await doc.getPage(1)
+            
+            // Setting the value of the page to String and assigning to a variable
+            let pageContent = await page.getTextContent()
+
+            let pdfLines = {}
+            let insIndex
+            let startIndex
+            const indexCondition = [2,28,52,65,67]
+            
+            //Mapping through the pdf lines
+            pageContent.items.map((item,index) => {
+
+                if(item.str !== ''){
+                    console.log(`Index: ${index}, Data: ${item.str}`)
+                    
+                    // Setting indexes for the instructions for
+                    if(item.str == 'Code:'){
+                        startIndex = index
+                        insIndex = index + 14
+                    }
+
+                    //Pushing to the array if the index is correct
+                    if(indexCondition.includes(index)){
+
+                        switch (index) {
+                            case 2:
+                                pdfLines['orderNumber'] = item.str
+                                break;
+                            case 28:
+                                pdfLines['dueDate'] = item.str.slice(10,20)
+                                break;
+
+                            case 52:
+                                pdfLines['lender'] = item.str
+                                break;
+
+                            case 65:
+                                pdfLines['address'] = item.str
+                            break;
+
+                            case 67:
+                                let citySlice= item.str.split(',')
+    
+                                console.log(citySlice);
+                                // citySlice.pop()
+                            
+                                citySlice[1].split(' ').map(
+                                    word =>{
+                                        if(word !== ''){
+                                            // console.log('Second: ',word)
+                                            citySlice.push(word)
+                                        }
+                            
+                                    }
+                                )
+
+                                // Fixing Formatting on Array
+                                citySlice.splice(1,1)
+                                // console.log(citySlice);
+                                
+                                // Adding the manual ids for the objects
+                                let manualId = ['city','state','zipCode']
+                                
+                                citySlice.map(
+                                    (word,index) => {
+                                        if(word !== ''){
+                                            pdfLines[manualId[index]] = word
+                                        }
+                                    }
+                                )
+                                
+                                // console.log(pdfLines);    
+                            
+                                // pdfLines.push({id:'lender',value: item.str})
+
+                            break;
+
+                            
+                        }
+                        
+                        
+                    }
+                }
+            })
+
+            
+            // this.$emit('get-data', pdfLines)
+            
+            this.pdfData = { ...pdfLines };
+            this.isChanged = true;
         }
     },
     
--- components/CustomInput.js
+++ components/CustomInput.js
@@ -1,133 +1,29 @@
 clkApp.component('custom-input',{
 
 
-    props: {
+    props:
+     {
         'id': String,
-        'type':String,
         'title':String,
         'customValue':String,
-        'handler':Boolean, 
+        // ['id','title','customValue'],
     },
     
     template: 
     /*html*/
     `
         <label :for="id">{{ title }} - {{ customValue }}</label><br>
-        <input v-if="handler" class="custom" 
-        @change="getContent" 
+        <input class="custom" 
         :id="id" 
-        :type="type"
         :value="customValue"
-        >
-        <input v-else class="custom" :id="id" :type="type" >
-        <br>
+        readonly><br>
+
     `,
 
 
     data() {
         return {
-        }
-    },
-
-    methods: {
-        async getContent(event){
-            console.log('Changed', event.target.files[0].name);
-            
-            // Getting the pdf file pages as a promise
-            const doc = await pdfjsLib.getDocument(`special/${event.target.files[0].name}`).promise // note the use of the property promise
-
-            // Getting the first page of the file
-            const page = await doc.getPage(1)
-            
-            // Setting the value of the page to String and assigning to a variable
-            let pageContent = await page.getTextContent()
-
-            let pdfLines = {}
-            let insIndex
-            let startIndex
-            const indexCondition = [2,28,52,65,67]
-            
-            //Mapping through the pdf lines
-            pageContent.items.map((item,index) => {
-
-                if(item.str !== ''){
-                    console.log(`Index: ${index}, Data: ${item.str}`)
-                    
-                    // Setting indexes for the instructions for
-                    if(item.str == 'Code:'){
-                        startIndex = index
-                        insIndex = index + 14
-                    }
-
-                    //Pushing to the array if the index is correct
-                    if(indexCondition.includes(index)){
-
-                        switch (index) {
-                            case 2:
-                                pdfLines['orderNumber'] = item.str
-                                break;
-                            case 28:
-                                pdfLines['dueDate'] = item.str.slice(10,20)
-                                break;
-
-                            case 52:
-                                pdfLines['lender'] = item.str
-                                break;
-
-                            case 65:
-                                pdfLines['address'] = item.str
-                            break;
-
-                            case 67:
-                                let citySlice= item.str.split(',')
-    
-                                console.log(citySlice);
-                                // citySlice.pop()
-                            
-                                citySlice[1].split(' ').map(
-                                    word =>{
-                                        if(word !== ''){
-                                            // console.log('Second: ',word)
-                                            citySlice.push(word)
-                                        }
-                            
-                                    }
-                                )
-
-                                // fixing Formating on Array
-                                citySlice.splice(1,1)
-                                // console.log(citySlice);
-                                
-                                // Adding the manual ids for the objects
-                                let manualId = ['city','state','zipCode']
-                                
-                                citySlice.map(
-                                    (word,index) => {
-                                        if(word !== ''){
-                                            pdfLines[manualId[index]] = word
-                                        }
-                                    }
-                                )
-                                
-                                console.log(pdfLines);    
-                            
-                                // pdfLines.push({id:'lender',value: item.str})
-
-                            break;
-
-                            default:
-                                break;
-                        }
-                        
-                        
-                    }
-                }
-            })
-
-            
-            this.$emit('get-data', pdfLines)
-            // this.inputData = pdfLines;
-
+            inputData: ''
         }
     },
 
