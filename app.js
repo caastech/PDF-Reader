ba0be5304c822da1a008a69844634504d4272017
Clemente Acosta <clems.acosta@gmail.com>
2021-11-15 15:22:09 -0500

    Changed pdf reading method
    Now the pdf file is read with the File Reader API, allowing to input a file from any directory in your machine. Also added animations to the head-title

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const clkApp = Vue.createApp({
     template: 
     /*html*/
     `
+    <h3>Note {{msg}}</h3>
     <label for="clkFile" class="file-label" :class="{ 'mb-2':isChanged }" >Please enter your PDF file</label>
     <input @change="getPdfData" type="file" name="clk" id="clkFile" class="input-file mb-2"><br>
     
     <div v-if="isChanged" class="container">
-
             <div class="details">
                 <custom-input v-for="(field,key) in fields" 
                 :key="key" 
@@ -47,7 +47,7 @@ const clkApp = Vue.createApp({
 
     data(){
         return{
-            // msg: 'Here I am',
+            msg: 'Helo',
             fields: [
                 { id:'dueDate', type:'text', title:'Due Date'},
                 { id:'clientPay', type:'text', title:'Client Pay'},
@@ -69,144 +69,173 @@ const clkApp = Vue.createApp({
     methods: {
 
         async getPdfData(event){
-            console.log('Changed', event.target.files[0].name);
+
+           
+            console.log('Changed', event.target.files[0]);
+
+            let file = event.target.files[0];
+            //Step 2: Read the file using file reader
+            let fileReader = new FileReader();  
             
-            // Getting the pdf file pages as a promise
-            const doc = await pdfjsLib.getDocument(`special/${event.target.files[0].name}`).promise // note the use of the property promise
-
-            // Getting the first page of the file
-            const page = await doc.getPage(1)
             
-            // Setting the value of the page to String and assigning to a variable
-            let pageContent = await page.getTextContent()
-
-            let pdfLines = {}
-            let startContent
-            let endContent
-            const indexCondition = [2,28,52,65,67]
             
-            //Mapping through the pdf lines
-            pageContent.items.map((item,index) => {
+            let pdfValue = 'Campus';
+            fileReader.onload = async function() {
 
-                if(item.str !== ''){
-                    console.log(`Index: ${index}, Data: ${item.str}`)
-                    
-                    // Setting indexes to get instructions data
-                    if(item.str == 'Code:'){
+                //Step 4:turn array buffer into typed array
+                let typedarray = new Uint8Array(this.result);
+                
+                // mountedApp.msg = pageContent.items[0]
+                
+                const doc = await pdfjsLib.getDocument(typedarray).promise // note the use of the property promise
 
-                        startContent = index + 1
-                        endContent = index + 14
+                // Getting the first page of the file
+                const page = await doc.getPage(1)
+                
+                // Setting the value of the page to String and assigning to a variable
+                let pageContent = await page.getTextContent()
 
-                    }
+                //Variables to get pdf data
+                let pdfLines = {}
+                let startContent
+                let endContent
+                const indexCondition = [2,28,52,65,67]
 
-                    //Pushing to the array if the index is correct
-                    if(indexCondition.includes(index)){
+                //Mapping through the pdf lines
+                pageContent.items.map((item,index) => {
 
-                        switch (index) {
-                            case 2:
-                                pdfLines['orderNumber'] = item.str
-                                break;
-                            case 28:
-                                pdfLines['dueDate'] = item.str.slice(10,20)
-                                break;
-
-                            case 52:
-                                pdfLines['lender'] = item.str
-                                break;
-
-                            case 65:
-                                pdfLines['address'] = item.str
-                            break;
-
-                            case 67:
-                                let citySlice= item.str.split(',')
-    
-                                // console.log('City Splite 1',citySlice);
-                            
-                                citySlice[1].split(' ').map(
-                                    word =>{
-                                        if(word !== ''){
-                                            // console.log('Second: ',word)
-                                            citySlice.push(word)
-                                        }
-                            
-                                    }
-                                )
-
-                                // Fix Formatting on Array
-                                citySlice.splice(1,1)
-                                // console.log('City Split 2',citySlice);
-                                
-                                // Add the manual ids for the objects
-                                let manualId = ['city','state','zip']
-                                
-                                citySlice.map(
-                                    (word,index) => {
-                                        if(word !== ''){
-                                            pdfLines[manualId[index]] = word
-                                        }
-                                    }
-                                )
-                                
-                            break;
-
-                            
-                        }
+                    if(item.str !== ''){
+                        console.log(`Index: ${index}, Data: ${item.str}`)
                         
-                        // Set the value of clientPay depending on the State
-                        switch (pdfLines['state']) {
-                            case 'NY':
-                                pdfLines['clientPay'] = '18.00';
+                        // Setting indexes to get instructions data
+                        if(item.str == 'Code:'){
+
+                            startContent = index + 1
+                            endContent = index + 14
+
+                        }
+
+                        //Pushing to the array if the index is correct
+                        if(indexCondition.includes(index)){
+
+                            switch (index) {
+                                case 2:
+                                    pdfLines['orderNumber'] = item.str
+                                    break;
+                                case 28:
+                                    pdfLines['dueDate'] = item.str.slice(10,20)
+                                    break;
+
+                                case 52:
+                                    pdfLines['lender'] = item.str
+                                    break;
+
+                                case 65:
+                                    pdfLines['address'] = item.str
                                 break;
 
-                            case 'HI':
-                                pdfLines['clientPay'] = '25.00';
-                                break;
-            
-                            case 'MI':
-                                pdfLines['clientPay'] = '25.00';
-                                break;
-            
-                            case 'PR':
-                                pdfLines['clientPay'] = '25.00';
-                                break;
+                                case 67:
+
+                                    let citySlice= item.str.split(',')
+        
+                                    // console.log('City Splite 1',citySlice);
+                                
+                                    citySlice[1].split(' ').map(
+                                        word =>{
+                                            if(word !== ''){
+                                                // console.log('Second: ',word)
+                                                citySlice.push(word)
+                                            }
+                                
+                                        }
+                                    )
+
+                                    // Fix Formatting on Array
+                                    citySlice.splice(1,1)
+                                    // console.log('City Split 2',citySlice);
                                     
-                            default:
-                                pdfLines['clientPay'] = '0.00';
+                                    // Add the manual ids for the objects
+                                    let manualId = ['city','state','zip']
+                                    
+                                    citySlice.map(
+                                        (word,index) => {
+                                            if(word !== ''){
+                                                pdfLines[manualId[index]] = word
+                                            }
+                                        }
+                                    )
+                                    
                                 break;
 
+                                
+                            }
+                            
+                            // Set the value of clientPay depending on the State
+                            switch (pdfLines['state']) {
+                                case 'NY':
+                                    pdfLines['clientPay'] = '18.00';
+                                    break;
+
+                                case 'HI':
+                                    pdfLines['clientPay'] = '25.00';
+                                    break;
+                
+                                case 'MI':
+                                    pdfLines['clientPay'] = '25.00';
+                                    break;
+                
+                                case 'PR':
+                                    pdfLines['clientPay'] = '25.00';
+                                    break;
+                                        
+                                default:
+                                    pdfLines['clientPay'] = '0.00';
+                                    break;
+
+                            }
+
+                            // Set Workcode Value (A static value for now)
+                            pdfLines['workCode'] = 'RUSH SPECIAL APPOINTMENT ORDER';
+                            
                         }
-
-                        // Set Workcode Value (A static value for now)
-                        pdfLines['workCode'] = 'RUSH SPECIAL APPOINTMENT ORDER';
-                        
                     }
-                }
-            })
+                })
 
-            pdfLines['instructions'] = ''
-            // For loop to set the instructions content
-            for (let line = startContent ; line <= endContent; line++) {
-                
-                if(pageContent.items[line].str !== ''){
+                pdfLines['instructions'] = ''
+
+                // For loop to set the instructions content
+                for (let line = startContent ; line <= endContent; line++) {
                     
+                    if(pageContent.items[line].str !== ''){
+                        
+                        
+                        console.log('Instructions Data:',pageContent.items[line].str)
+                        pdfLines['instructions'] += `${pageContent.items[line].str} \n`
+
+                    }
                     
-                    console.log('Instructions Data:',pageContent.items[line].str)
-                    pdfLines['instructions'] += `${pageContent.items[line].str} \n`
-
                 }
-                
-            }
 
-            // Set original instructions to lower case and assign it to the variable to print
-            this.originalTextArea = pdfLines['instructions'].toLowerCase()             
+                mountedApp.msg = 'Sopa de Macaou 2.0'
 
-            // Set all the data from the pdf to the variable to print
-            this.pdfData = { ...pdfLines };
+                // Set original instructions to lower case and assign it to the variable to print
+                mountedApp.originalTextArea = pdfLines['instructions'].toLowerCase()             
 
-            // Set to true if the input file has changed
-            this.isChanged = true;
+                // Set all the data from the pdf to the variable to print
+                mountedApp.pdfData = { ...pdfLines };
+
+                // Set to true if the input file has changed
+                mountedApp.isChanged = true;
+
+            
+            };
+    
+            fileReader.readAsArrayBuffer(file);
+
+
+            
         },
+
 
         setTextArea(text){
             this.modifiedTextArea = text;
